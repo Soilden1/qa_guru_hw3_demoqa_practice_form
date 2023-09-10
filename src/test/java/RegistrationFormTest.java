faeb689bbe8140c38f8460ef5d9f78b1b72ca1f4
Dmitry_Smirnov <dimacm14@gmail.com>
2023-09-11 02:25:37 +0300

    Made the values constants

--- src/test/java/RegistrationFormTest.java
+++ src/test/java/RegistrationFormTest.java
@@ -13,6 +13,22 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class RegistrationFormTest {
 
+    private static final String FIRST_NAME = "Ivan";
+    private static final String LAST_NAME = "Ivanov";
+    private static final String EMAIL = "IvanovIvan123@gmail.com";
+    private static final String GENDER = "Male";
+    private static final String PHONE_NUMBER = "8005553535";
+    private static final String DATE_OF_BIRTH = "07 January,2004";
+    private static final String SUBJECT1 = "Computer Science";
+    private static final String SUBJECT2 = "English";
+    private static final String HOBBY1 = "Reading";
+    private static final String HOBBY2 = "Music";
+    private static final File PICTURE = new File("resources/human.png");
+    private static final String CURRENT_ADDRESS = "Pushkin street, house 333";
+    private static final String STATE = "NCR";
+    private static final String CITY = "Delhi";
+    private static final String COMPLETE_SUBMIT_MESSAGE = "Thanks for submitting the form";
+
     @BeforeAll
     static void setUp() {
         Configuration.pageLoadStrategy = "eager";
@@ -23,40 +39,40 @@ public class RegistrationFormTest {
     @Test
     void RegistrationTest() {
         open("/automation-practice-form");
-        $("#firstName").setValue("Ivan");
-        $("#lastName").setValue("Ivanov");
-        $("#userEmail").setValue("IvanovIvan123@gmail.com");
-        $("#genterWrapper").$(byText("Male")).click();
-        $("#userNumber").setValue("8005553535");
+        $("#firstName").setValue(FIRST_NAME);
+        $("#lastName").setValue(LAST_NAME);
+        $("#userEmail").setValue(EMAIL);
+        $("#genterWrapper").$(byText(GENDER)).click();
+        $("#userNumber").setValue(PHONE_NUMBER);
 
         $("#dateOfBirthInput").sendKeys(Keys.CONTROL, "A");
-        $("#dateOfBirthInput").sendKeys("07 Jan 2004", Keys.ENTER);
+        $("#dateOfBirthInput").sendKeys(DATE_OF_BIRTH, Keys.ENTER);
 
-        $("#subjectsInput").setValue("Computer Science").sendKeys(Keys.ENTER);
-        $("#subjectsInput").setValue("English").sendKeys(Keys.ENTER);
+        $("#subjectsInput").setValue(SUBJECT1).sendKeys(Keys.ENTER);
+        $("#subjectsInput").setValue(SUBJECT2).sendKeys(Keys.ENTER);
 
-        $("#hobbiesWrapper").$(byText("Reading")).click();
-        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#hobbiesWrapper").$(byText(HOBBY1)).click();
+        $("#hobbiesWrapper").$(byText(HOBBY2)).click();
 
-        $("#uploadPicture").uploadFile(new File("resources/human.png"));
-        $("#currentAddress").setValue("Pushkin street, house 333");
-        $("#stateCity-wrapper #state input").setValue("NCR").sendKeys(Keys.ENTER);
-        $("#stateCity-wrapper #city input").setValue("Delhi").sendKeys(Keys.ENTER);
+        $("#uploadPicture").uploadFile(PICTURE);
+        $("#currentAddress").setValue(CURRENT_ADDRESS);
+        $("#stateCity-wrapper #state input").setValue(STATE).sendKeys(Keys.ENTER);
+        $("#stateCity-wrapper #city input").setValue(CITY).sendKeys(Keys.ENTER);
         $("#submit").click();
 
         SelenideElement table = $(".table-responsive .table");
         List<SelenideElement> rows = table.findAll("tr");
 
-        $(".modal-header .modal-title").shouldHave(text("Thanks for submitting the form"));
-        rows.get(1).shouldHave(text("Ivan Ivanov"));
-        rows.get(2).shouldHave(text("IvanovIvan123@gmail.com"));
-        rows.get(3).shouldHave(text("Male"));
-        rows.get(4).shouldHave(text("8005553535"));
-        rows.get(5).shouldHave(text("07 January,2004"));
-        rows.get(6).shouldHave(text("Computer Science, English"));
-        rows.get(7).shouldHave(text("Reading, Music"));
-        rows.get(8).shouldHave(text("human.png"));
-        rows.get(9).shouldHave(text("Pushkin street, house 333"));
-        rows.get(10).shouldHave(text("NCR Delhi"));
+        $(".modal-header .modal-title").shouldHave(text(COMPLETE_SUBMIT_MESSAGE));
+        rows.get(1).shouldHave(text(FIRST_NAME + " " + LAST_NAME));
+        rows.get(2).shouldHave(text(EMAIL));
+        rows.get(3).shouldHave(text(GENDER));
+        rows.get(4).shouldHave(text(PHONE_NUMBER));
+        rows.get(5).shouldHave(text(DATE_OF_BIRTH));
+        rows.get(6).shouldHave(text(SUBJECT1 + ", " + SUBJECT2));
+        rows.get(7).shouldHave(text(HOBBY1 + ", " + HOBBY2));
+        rows.get(8).shouldHave(text(PICTURE.getName()));
+        rows.get(9).shouldHave(text(CURRENT_ADDRESS));
+        rows.get(10).shouldHave(text(STATE + " " + CITY));
     }
 }
